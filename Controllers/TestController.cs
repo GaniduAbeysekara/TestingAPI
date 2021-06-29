90802ed09345bba975ad1b54502006fc9f215823
GaniduAbeysekara <ganiduofficial@gmail.com>
2021-06-29 21:50:16 +0530

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Kaushi", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        [Route("kaushi")]
+        public ActionResult Testing()
+        {
+
+            return Ok(Summaries);
+        }
+    }
+}
