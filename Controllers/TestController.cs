8ee3baf1a8880031e4efb32fd8c4bd8e3452173c
GaniduAbeysekara <ganiduofficial@gmail.com>
2021-06-29 22:20:08 +0530

    create a model and post method

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TestingAPI.Models;
 
 namespace TestingAPI.Controllers
 {
@@ -27,6 +28,17 @@ namespace TestingAPI.Controllers
         {
 
             return Ok(Summaries);
+        }
+
+        [HttpPost]
+        [Route("create/kaushi")]
+        public ActionResult testPost(Customer cus)
+        {
+            if (cus == null)
+            {
+                return BadRequest("404");
+            }
+            return Ok(cus);
         }
     }
 }
---
+++ Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+    }
+}
